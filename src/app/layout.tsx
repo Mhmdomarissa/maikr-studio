c63732072cdd121103f48cfb7247d3eb3ef064c6
Mhmdomarissa <mhmdomarissa1@gmail.com>
2026-02-24 05:46:04 +0200

    Add Google Analytics 4 (G-T4ZW1XKXMC) via @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -120,6 +121,7 @@ export default function RootLayout({
         />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-T4ZW1XKXMC" />
     </html>
   );
 }
